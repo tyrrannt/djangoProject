23c30a328dd147bab4d6bc9b8553fff09507f837
shakirov <shakirov.vitaliy@gmail.com>
2022-07-04 18:07:45 +0300

    123

--- customers_app/admin.py
+++ customers_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
-from .models import DataBaseUser
+from .models import DataBaseUser, City, Country, Category, Address, AccessLevel, PhoneNumber, Job, Division, Work
 
 
 class CustomUserAdmin(UserAdmin):
@@ -34,3 +34,11 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(DataBaseUser, CustomUserAdmin)
+admin.site.register(City,)
+admin.site.register(Country,)
+admin.site.register(Address,)
+admin.site.register(AccessLevel,)
+admin.site.register(PhoneNumber,)
+admin.site.register(Job,)
+admin.site.register(Division,)
+admin.site.register(Work)
