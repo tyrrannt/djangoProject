718969e1bbf56254d2e252fa0bea06987e895937
shakirov <shakirov.vitaliy@gmail.com>
2022-08-05 17:03:36 +0300

    Устранены ошибки доступа незарегистрированного пользователя

--- customers_app/views.py
+++ customers_app/views.py
@@ -15,6 +15,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request, 'customers_app/main.html')
 
