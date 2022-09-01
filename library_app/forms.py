97059bd1ca835d4f6424beb8dd4d75cec28d3737
Work: shakirov <shakirov.vitaliy@gmail.com>
2022-09-01 12:23:12 +0300

    Внесены корректировки в дизайн

--- library_app/forms.py
+++ library_app/forms.py
@@ -8,7 +8,7 @@ class DocumentsAddForm(forms.ModelForm):
         model = Documents
         fields = ('type_of_document', 'executor', 'document_date', 'document_number', 'doc_file', 'access',
                   'document_division', 'employee', 'allowed_placed', 'validity_period_start',
-                  'validity_period_end', 'actuality', 'previous_document')
+                  'validity_period_end', 'actuality', 'previous_document', 'document_name')
 
 
 class DocumentsUpdateForm(forms.ModelForm):
@@ -16,6 +16,6 @@ class DocumentsUpdateForm(forms.ModelForm):
         model = Documents
         fields = ('type_of_document', 'executor', 'document_date', 'document_number', 'doc_file', 'access',
                   'document_division', 'employee', 'validity_period_start', 'validity_period_end', 'previous_document',
-                  'allowed_placed', 'actuality')
+                  'allowed_placed', 'actuality', 'document_name')
 
 
