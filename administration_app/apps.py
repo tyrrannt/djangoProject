0698f8ba1e0e96e3a3db4684160e1628b38ca481
msi <shakirov.vitaliy@gmail.com>
2023-03-27 11:11:36 +0300

    Обновлены все пакеты

--- administration_app/apps.py
+++ administration_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AdministrationAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'administration_app'
+    verbose_name = 'Административная часть'
--- hrdepartment_app/apps.py
+++ hrdepartment_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class HrdepartmentAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hrdepartment_app'
+    verbose_name = 'Кадровые документы'
