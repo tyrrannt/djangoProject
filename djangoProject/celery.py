d1e752e4b412035e0667c0e41403413f71184456
msi <shakirov.vitaliy@gmail.com>
2023-05-12 23:00:52 +0300

    Обновлены все пакеты

--- djangoProject/celery.py
+++ djangoProject/celery.py
@@ -8,9 +8,9 @@ app = Celery('djangoProject')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-app.conf.beat_schedule = {
-    'send-spam': {
-        'task': 'hrdepartment_app.tasks.report_card_separator',
-        'schedule': crontab(minute=10, hour=0),
-    }
-}
+# app.conf.beat_schedule = {
+#     'send-spam': {
+#         'task': 'hrdepartment_app.tasks.report_card_separator',
+#         'schedule': crontab(minute=10, hour=0),
+#     }
+# }
