98ad838fd2f64c95d8630e342692b9529ec2384a
msi <shakirov.vitaliy@gmail.com>
2023-05-17 09:40:50 +0300

    Обновлены все пакеты

--- djangoProject/celery.py
+++ djangoProject/celery.py
@@ -12,7 +12,8 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'load_report_card': {
         'task': 'hrdepartment_app.tasks.report_card_separator',
-        'schedule': crontab(minute=0, hour=7),
+        # 'schedule': crontab(minute=0, hour=7),
+        'schedule': crontab(minute='*/5'),
     },
     'birthday_gift': {
         'task': 'hrdepartment_app.tasks.happy_birthday',
