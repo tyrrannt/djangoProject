451efd0fe4b55b7670bffea26351deec6d49ab0a
msi <shakirov.vitaliy@gmail.com>
2023-05-31 13:40:35 +0300

    Обновлены все пакеты

--- telegram_app/urls.py
+++ telegram_app/urls.py
@@ -3,6 +3,6 @@ from .views import home, telegram
 from djangoProject.settings import WEBHOOK_PATH
 app_name = 'telegram_app'
 urlpatterns = [
-    # path('', home, name='home'),
-    # path(WEBHOOK_PATH, telegram, name='webhook'),
+    path('', home, name='home'),
+    path(WEBHOOK_PATH, telegram, name='webhook'),
 ]
