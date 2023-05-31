04718fa3416fee96b81912b85a6a470b79344425
msi <shakirov.vitaliy@gmail.com>
2023-05-31 14:21:07 +0300

    Обновлены все пакеты

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -284,5 +284,5 @@ INTERNAL_IPS = [
 CKEDITOR_5_FILE_STORAGE = 'administration_app.utils.CkeditorCustomStorage'
 
 API_TOKEN = config('TELEGRAM_TOKEN')
-WEBHOOK_PATH = 'telegram_app/' + hashlib.md5(API_TOKEN.encode()).hexdigest()
+WEBHOOK_PATH = 'telegram_app/' + API_TOKEN
 WEBHOOK_URL = f"corp.barkol.ru/{WEBHOOK_PATH}"
