e4aff22b7fa894ba2cd40fbf0f6f01250260d828
msi <shakirov.vitaliy@gmail.com>
2023-06-06 18:11:37 +0300

    Обновлены все пакеты

--- hrdepartment_app/views.py
+++ hrdepartment_app/views.py
@@ -1386,7 +1386,7 @@ class ReportCardList(LoginRequiredMixin, ListView):
                 start_date = datetime.date(year=int(request.session['current_year']), month=int(request.session['current_month']), day=1)
                 end_date = start_date + relativedelta(days=31)
                 search_interval = list(rrule.rrule(rrule.DAILY, dtstart=start_date, until=end_date))
-                reportcard_list = ReportCard.objects.filter(Q(employee=self.request.user) & Q(record_type='13') & Q(report_card_day__in=search_interval)).order_by('report_card_day')
+                reportcard_list = ReportCard.objects.filter(Q(employee=self.request.user) & Q(record_type__in=['1', '13']) & Q(report_card_day__in=search_interval)).order_by('report_card_day')
             else:
                 reportcard_list = ReportCard.objects.filter(Q(employee=self.request.user) & Q(record_type='13')).order_by('report_card_day')
             data = [reportcard_item.get_data() for reportcard_item in reportcard_list]
