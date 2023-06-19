f2903a58e70baeafbacdffc460fabeb021a8ba07
msi <shakirov.vitaliy@gmail.com>
2023-06-19 14:45:53 +0300

    Обновлены все пакеты

--- library_app/models.py
+++ library_app/models.py
@@ -1,13 +1,17 @@
+import pathlib
 import uuid
 
 from decouple import config
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.urls import reverse
 from django_ckeditor_5.fields import CKEditor5Field
 from loguru import logger
 
 from customers_app.models import DataBaseUser
+from djangoProject.settings import BASE_DIR
 
 logger.add("debug.json", format=config('LOG_FORMAT'), level=config('LOG_LEVEL'),
            rotation=config('LOG_ROTATION'), compression=config('LOG_COMPRESSION'),
@@ -92,3 +96,40 @@ class DocumentForm(models.Model):
 
     def __str__(self):
         return self.title
+
+@receiver(post_save, sender=DocumentForm)
+def rename_file_name(sender, instance, **kwargs):
+    try:
+        change = 0
+        if instance.draft:
+            # Получаем имя сохраненного файла
+            draft_file_name = pathlib.Path(instance.draft.name).name
+            # Получаем путь к файлу
+            draft_path_name = pathlib.Path(instance.draft.name).parent
+            # Получаем расширение файла
+            draft_ext = draft_file_name.split('.')[-1]
+            filename_draft = f'BLK-{instance.ref_key}-DRAFT.{draft_ext}'
+            if draft_file_name != filename_draft:
+                pathlib.Path.rename(pathlib.Path.joinpath(BASE_DIR, 'media', draft_path_name, draft_file_name),
+                                    pathlib.Path.joinpath(BASE_DIR, 'media', draft_path_name, filename_draft))
+                instance.draft = f'{draft_path_name}/{filename_draft}'
+                change = 1
+
+        if instance.scan:
+            # Получаем имя сохраненного файла
+            scan_file_name = pathlib.Path(instance.scan.name).name
+            # Получаем путь к файлу
+            scan_path_name = pathlib.Path(instance.scan.name).parent
+            # Получаем расширение файла
+            scan_ext = scan_file_name.split('.')[-1]
+            filename_scan = f'BLK-{instance.ref_key}-SCAN.pdf'
+            if scan_file_name != filename_scan:
+                pathlib.Path.rename(pathlib.Path.joinpath(BASE_DIR, 'media', scan_path_name, scan_file_name),
+                                    pathlib.Path.joinpath(BASE_DIR, 'media', scan_path_name, filename_scan))
+                instance.scan = f'{scan_path_name}/{filename_scan}'
+                change = 1
+
+        if change == 1:
+            instance.save()
+    except Exception as _ex:
+        logger.error(f'Ошибка при переименовании файла {_ex}')
