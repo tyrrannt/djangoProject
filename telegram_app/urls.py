407474a84c593b07d8f299b94ef3b75c5c44bcad
msi <shakirov.vitaliy@gmail.com>
2023-06-23 22:19:49 +0300

    Обновлены все пакеты

--- telegram_app/urls.py
+++ telegram_app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import home, telegram
-from djangoProject.settings import WEBHOOK_PATH
+
 app_name = 'telegram_app'
 urlpatterns = [
-    path('', home, name='home'),
-    path(WEBHOOK_PATH, telegram, name='webhook'),
+    # path('', home, name='home'),
+    # path(WEBHOOK_PATH, telegram, name='webhook'),
 ]
--- telegram_app/views.py
+++ telegram_app/views.py
@@ -1,24 +1,2 @@
 from django.shortcuts import render
-from asgiref.sync import async_to_sync
-from loguru import logger
 
-from .webhook import proceed_update
-from django.http import HttpResponse, HttpRequest
-from django.views.decorators.csrf import csrf_exempt
-
-logger.add("debug.json", format="{time} {level} {message}", level="DEBUG", rotation="10 MB", compression="zip",
-           serialize=True)
-def home(request: HttpRequest):
-    return HttpResponse('Hello world')
-
-@csrf_exempt
-def telegram(request: HttpRequest):
-    # if request.method == 'post':
-    try:
-        async_to_sync(proceed_update)(request)
-        logger.error(f'Функция telegram: {request}')
-    except Exception as e:
-        logger.error(f'Функция telegram, исключение: {request}')
-    return HttpResponse()
-    # else:
-    #     return HttpResponse(status=403)
