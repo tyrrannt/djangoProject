b452831ebd3526b1e87295163a929d5595a53fe9
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2023-08-04 18:49:58 +0300

    Improve text formatting and clean up code
    Added an 'alignment' feature in settings.py to provide additional text formatting options. Refactored hrdepartment_app/models.py and administration_app/views.py to improve readability and removed a redundant print statement from models.py. Also added 'Document' from 'docx' and 'HtmlToDocx' from 'htmldocx processing in models.py.
    
    These changes aim to enhance the applications user interface by providing better text formatting options and improve overall maintenance by making the codebase cleaner and more readable.

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -227,7 +227,7 @@ CKEDITOR_5_CONFIGS = {
                 "subscript",
                 "superscript",
                 "highlight",
-                "alignment",
+                # "alignment",
                 "|",
                 "codeBlock",
                 "sourceEditing",
