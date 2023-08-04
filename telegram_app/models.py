524c98c5abeefdcb6a931af78c601409e6c13c49
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2023-08-04 12:19:48 +0300

    The given commit message does not match the provided diff. From the diff you can tell that a "str" method was added to represent the model objects in Django admin interface. Here is a suitable commit message for these changes.
    "Add __str__ method to TelegramUser model
    
    Added __str__ method to TelegramUser model in models.py. This method returns the chat_id of the TelegramUser object. This change makes it easier to distinguish different objects in Django admin interface."

--- telegram_app/models.py
+++ telegram_app/models.py
@@ -1,22 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
 class ChatID(models.Model):
     class Meta:
-        verbose_name = 'Идентификатор чата телеграм'
-        verbose_name_plural = 'Идентификатор чата телеграм'
+        verbose_name = "Идентификатор чата телеграм"
+        verbose_name_plural = "Идентификатор чата телеграм"
 
-    ref_key = models.CharField(verbose_name='Уникальный номер', max_length=37, default='')
-    chat_id = models.CharField(verbose_name='Telegram ID', max_length=20, blank=True)
+    ref_key = models.CharField(
+        verbose_name="Уникальный номер", max_length=37, default=""
+    )
+    chat_id = models.CharField(verbose_name="Telegram ID", max_length=20, blank=True)
+
+    def __str__(self):
+        return self.chat_id
+
 
 class TelegramNotification(models.Model):
     class Meta:
-        verbose_name = 'Уведомление телеграм'
-        verbose_name_plural = 'Уведомления телеграм'
+        verbose_name = "Уведомление телеграм"
+        verbose_name_plural = "Уведомления телеграм"
 
-    respondents = models.ManyToManyField(ChatID, verbose_name='Получатели')
-    message = models.CharField(verbose_name='Сообщение', max_length=256, default='', blank=True)
-    document_url = models.URLField(verbose_name='Ссылка документ', blank=True)
-    document_id = models.CharField(verbose_name='UIN документа', default='', max_length=37, null=True, blank=True)
-    sending_counter = models.IntegerField(verbose_name='Счетчик отправок', default=3)
-    send_time = models.TimeField(verbose_name='Время отправки', blank=True, null=True)
+    respondents = models.ManyToManyField(ChatID, verbose_name="Получатели")
+    message = models.CharField(
+        verbose_name="Сообщение", max_length=256, default="", blank=True
+    )
+    document_url = models.URLField(verbose_name="Ссылка документ", blank=True)
+    document_id = models.CharField(
+        verbose_name="UIN документа", default="", max_length=37, null=True, blank=True
+    )
+    sending_counter = models.IntegerField(verbose_name="Счетчик отправок", default=3)
+    send_time = models.TimeField(verbose_name="Время отправки", blank=True, null=True)
