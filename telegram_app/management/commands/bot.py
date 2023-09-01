9416f3b0560e59230c63bc3e7d53323848c70fc6
Shakirov Vitaliy <shakirov.vitaliy@gmail.com>
2023-09-01 17:19:06 +0300

    Wrapped bot initialization in try-except block
    The initialization of telegram bot has been wrapped in a try-except block to handle any exceptions that may occur during its initialization, and notify the developer with the relevant error message. This will improve the error handling mechanism and provide more detailed information when a failure occurs, making it easier to perform debugging and troubleshooting.

--- telegram_app/management/commands/bot.py
+++ telegram_app/management/commands/bot.py
@@ -16,19 +16,24 @@ from customers_app.models import DataBaseUser
 from djangoProject.settings import API_TOKEN
 from telegram_app.models import ChatID, TelegramNotification
 
-action = ['ПОДПИСАТЬСЯ', 'ПРОВЕРИТЬ']
-author_action = ['Количество']
+action = ["ПОДПИСАТЬСЯ", "ПРОВЕРИТЬ"]
+author_action = ["Количество"]
 article_action = []
 
-logger.add("debug.json", format=config('LOG_FORMAT'), level=config('LOG_LEVEL'),
-           rotation=config('LOG_ROTATION'), compression=config('LOG_COMPRESSION'),
-           serialize=config('LOG_SERIALIZE'))
+logger.add(
+    "debug.json",
+    format=config("LOG_FORMAT"),
+    level=config("LOG_LEVEL"),
+    rotation=config("LOG_ROTATION"),
+    compression=config("LOG_COMPRESSION"),
+    serialize=config("LOG_SERIALIZE"),
+)
 
 
 def main_bot(tok):
     bot = telebot.TeleBot(tok, skip_pending=True)
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('ПОЛЬЗОВАТЕЛИ', 'ПОДПИСКА')
+    keyboard.row("ПОЛЬЗОВАТЕЛИ", "ПОДПИСКА")
     subscribe_button, author_button, article_button = [], [], []
 
     otvet = telebot.types.InlineKeyboardMarkup(row_width=2)
@@ -36,13 +41,23 @@ def main_bot(tok):
     article_otvet = telebot.types.InlineKeyboardMarkup(row_width=2)
 
     for item in range(0, len(action)):
-        subscribe_button.append(telebot.types.InlineKeyboardButton(f"{action[item]}", callback_data=action[item]))
+        subscribe_button.append(
+            telebot.types.InlineKeyboardButton(
+                f"{action[item]}", callback_data=action[item]
+            )
+        )
     for item in range(0, len(author_action)):
         author_button.append(
-            telebot.types.InlineKeyboardButton(f"{author_action[item]}", callback_data=author_action[item]))
+            telebot.types.InlineKeyboardButton(
+                f"{author_action[item]}", callback_data=author_action[item]
+            )
+        )
     for item in range(0, len(article_action)):
         article_button.append(
-            telebot.types.InlineKeyboardButton(f"{article_action[item]}", callback_data=article_action[item]))
+            telebot.types.InlineKeyboardButton(
+                f"{article_action[item]}", callback_data=article_action[item]
+            )
+        )
 
     for item in range(0, len(action)):
         otvet.add(subscribe_button[item])
@@ -54,26 +69,39 @@ def main_bot(tok):
     @bot.message_handler(commands=["start"])
     def start_message(message):
         print(message.chat.id)
-        bot.send_message(message.chat.id,
-                         f"Здравствуйте {message.from_user.first_name}, для продолжения воспользуйтесь меню.",
-                         reply_markup=keyboard)
+        bot.send_message(
+            message.chat.id,
+            f"Здравствуйте {message.from_user.first_name}, для продолжения воспользуйтесь меню.",
+            reply_markup=keyboard,
+        )
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_inline(call):
         try:
             if call.message:
-                if call.data == 'ПОДПИСАТЬСЯ':
-                    bot.send_message(call.message.chat.id, 'Отправь УИН из своего профиля')
-                if call.data == 'ПРОВЕРИТЬ':
+                if call.data == "ПОДПИСАТЬСЯ":
+                    bot.send_message(
+                        call.message.chat.id, "Отправь УИН из своего профиля"
+                    )
+                if call.data == "ПРОВЕРИТЬ":
                     if DataBaseUser.objects.filter(telegram_id=call.message.chat.id):
-                        bot.send_message(call.message.chat.id, 'Вы успешно подписаны на уведомления! ')
+                        bot.send_message(
+                            call.message.chat.id,
+                            "Вы успешно подписаны на уведомления! ",
+                        )
                     else:
-                        bot.send_message(call.message.chat.id,
-                                         'Не нашел вас в списке пользователей! Пройдите процес подписки на уведомления.')
-                if call.data == 'Количество':
-                    msg = DataBaseUser.objects.all().exclude(telegram_id='')
-                    message_to_user = f'Количество подписанных пользователей = {msg.count()}'
-                    bot.send_message(call.message.chat.id, message_to_user, parse_mode='HTML')
+                        bot.send_message(
+                            call.message.chat.id,
+                            "Не нашел вас в списке пользователей! Пройдите процес подписки на уведомления.",
+                        )
+                if call.data == "Количество":
+                    msg = DataBaseUser.objects.all().exclude(telegram_id="")
+                    message_to_user = (
+                        f"Количество подписанных пользователей = {msg.count()}"
+                    )
+                    bot.send_message(
+                        call.message.chat.id, message_to_user, parse_mode="HTML"
+                    )
 
         except Exception as e:
             print(repr(e))
@@ -83,33 +111,47 @@ def main_bot(tok):
         if len(message.text) == 36:
             if DataBaseUser.objects.filter(person_ref_key=message.text):
                 user_obj = DataBaseUser.objects.get(person_ref_key=message.text)
-                ChatID.objects.update_or_create(chat_id=message.chat.id, ref_key=user_obj.person_ref_key)
+                ChatID.objects.update_or_create(
+                    chat_id=message.chat.id, ref_key=user_obj.person_ref_key
+                )
                 user_obj.telegram_id = message.chat.id
                 user_obj.save()
-                bot.send_message(message.chat.id, 'Вы успешно подписаны на уведомления! ')
+                bot.send_message(
+                    message.chat.id, "Вы успешно подписаны на уведомления! "
+                )
             else:
-                bot.send_message(message.chat.id, 'Не нашел вас в списке пользователей! ')
+                bot.send_message(
+                    message.chat.id, "Не нашел вас в списке пользователей! "
+                )
         if message.text.lower() == "пользователи":
-            bot.send_message(message.chat.id, 'Выберите вариант: ', reply_markup=author_otvet)
+            bot.send_message(
+                message.chat.id, "Выберите вариант: ", reply_markup=author_otvet
+            )
 
         if message.text.lower() == "статьи":
-            bot.send_message(message.chat.id, 'Выберите категорию: ', reply_markup=article_otvet)
+            bot.send_message(
+                message.chat.id, "Выберите категорию: ", reply_markup=article_otvet
+            )
 
         if message.text.lower() == "подписка":
-            bot.send_message(message.chat.id, 'Выберите вариант:', reply_markup=otvet)
+            bot.send_message(message.chat.id, "Выберите вариант:", reply_markup=otvet)
 
         if message.text.lower()[:1] == "@":
             check_email = parseaddr(message.text.lower()[1:])
-            if check_email[1] != '':
+            if check_email[1] != "":
                 verify_link = f"/telegram/{message.chat.id}:{check_email[1]}/"
 
-                title = f'Подтверждение учетной записи {message.chat.id}'
-                email_message = f'Для подтверждения учетной записи {message.chat.id}' \
-                                f' на портале https://reqsoft.ru перейдите по ' \
-                                f'ссылке: \nhttps://reqsoft.ru{verify_link} '
-                bot_message = f'Для подтверждения учетной записи {message.chat.id}' \
-                              f' на портале https://reqsoft.ru перейдите по ' \
-                              f'ссылке отправленной вам на email '
+                title = f"Подтверждение учетной записи {message.chat.id}"
+                email_message = (
+                    f"Для подтверждения учетной записи {message.chat.id}"
+                    f" на портале https://reqsoft.ru перейдите по "
+                    f"ссылке: \nhttps://reqsoft.ru{verify_link} "
+                )
+                bot_message = (
+                    f"Для подтверждения учетной записи {message.chat.id}"
+                    f" на портале https://reqsoft.ru перейдите по "
+                    f"ссылке отправленной вам на email "
+                )
                 # send_mail(title, email_message, settings.EMAIL_HOST_USER, [check_email[1]], fail_silently=False,
                 #           auth_user=settings.EMAIL_HOST_USER, auth_password=settings.EMAIL_HOST_PASSWORD)
                 bot.send_message(message.chat.id, f"{bot_message}")
@@ -123,31 +165,47 @@ def send_message_tg():
     result = list()
     try:
         bot = telebot.TeleBot(API_TOKEN, skip_pending=True)
-        notify_list = TelegramNotification.objects.filter(Q(send_time__hour=dt.hour) & Q(send_time__minute=dt.minute))
+        notify_list = TelegramNotification.objects.filter(
+            Q(send_time__hour=dt.hour) & Q(send_time__minute=dt.minute)
+        )
         for item in notify_list:
             for chat_id in item.respondents.all():
                 if item.sending_counter > 0:
                     if item.document_url:
-                        bot.send_message(chat_id.chat_id, f'{item.message}. <a href="{item.document_url}">Ссылка на '
-                                                          f'документ</a>', parse_mode='HTML')
-                        result.append(f'Сообщение для {chat_id.chat_id}: {item.message}. '
-                                      f'Ссылка на документ: {item.document_url}')
-                        logger.info(f'Сообщение для {chat_id.chat_id} отправлено. '
-                                    f'Текст: {item.message}. Ссылка: {item.document_url}')
+                        bot.send_message(
+                            chat_id.chat_id,
+                            f'{item.message}. <a href="{item.document_url}">Ссылка на '
+                            f"документ</a>",
+                            parse_mode="HTML",
+                        )
+                        result.append(
+                            f"Сообщение для {chat_id.chat_id}: {item.message}. "
+                            f"Ссылка на документ: {item.document_url}"
+                        )
+                        logger.info(
+                            f"Сообщение для {chat_id.chat_id} отправлено. "
+                            f"Текст: {item.message}. Ссылка: {item.document_url}"
+                        )
                     else:
-                        bot.send_message(chat_id.chat_id, f'{item.message}', parse_mode='HTML')
-                        result.append(f'Сообщение для {chat_id.chat_id}: {item.message}.')
-                        logger.info(f'Сообщение для {chat_id.chat_id} отправлено. Текст: {item.message}.')
+                        bot.send_message(
+                            chat_id.chat_id, f"{item.message}", parse_mode="HTML"
+                        )
+                        result.append(
+                            f"Сообщение для {chat_id.chat_id}: {item.message}."
+                        )
+                        logger.info(
+                            f"Сообщение для {chat_id.chat_id} отправлено. Текст: {item.message}."
+                        )
             item.sending_counter -= 1
             item.send_time = dt + relativedelta(minutes=time_list[item.sending_counter])
             item.save()
     except Exception as _ex:
-        result.append(f'Ошибка telegram бота: {_ex}')
+        result.append(f"Ошибка telegram бота: {_ex}")
     return result
 
 
 class Command(BaseCommand):
-    help = 'Запускет бота'
+    help = "Запускет бота"
 
     def handle(self, *args, **options):
         main_bot(API_TOKEN)
