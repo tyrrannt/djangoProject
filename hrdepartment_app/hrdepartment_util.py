6e9cfef226d7626aba21e99dacc4faee4f1a085f
msi <shakirov.vitaliy@gmail.com>
2023-12-27 21:08:13 +0300

    Implemented pre-holiday hours computation in hrdepartment_app
    Modified the method to calculate work hours in a month in 'hrdepartment_app/models.py' to include pre-holiday hours. The previous calculation did not account for working days that are pre-holidays with lesser work hours. Now, the function calculates the number of pre-holiday days within a month and their corresponding work hours, which are lower than the regular work hours.
    
    Also, removed unnecessary print statements from 'hrdepartment_app/forms.py' for memo creation, enhancing code readability and debugging process. Specific error messages were introduced for improved data validation.

--- hrdepartment_app/hrdepartment_util.py
+++ hrdepartment_app/hrdepartment_util.py
@@ -38,7 +38,7 @@ def get_medical_documents():
     # ToDo: Счетчик добавленных контрагентов из 1С. Подумать как передать его значение
     for item in todos["value"]:
         if item["Posted"]:
-            db_user = db_users.filter(person_ref_key=item["ФизическоеЛицо_Key"])
+            db_user = db_users.filter(person_ref_key=item["ФизическоеЛицо_Key"]).exclude(is_active=False)
             db_med_org = item["МедицинскаяОрганизация_Key"]
             if (
                     db_user.count() > 0
