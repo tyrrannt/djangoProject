7e9193e969101ecef07da768a4a0390e39c364af
msi <shakirov.vitaliy@gmail.com>
2024-03-31 14:46:46 +0300

    Update module

--- logistics_app/urls.py
+++ logistics_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from logistics_app.views import WayBillListView, WayBillCreateView, WayBillUpdateView, WayBillDetailView, WayBillDeleteView
+from logistics_app.views import (WayBillListView, WayBillCreateView, WayBillUpdateView,
+                                 WayBillDetailView, WayBillDeleteView)
 
 app_name = 'logistics_app'
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     path('waybill/', WayBillListView.as_view(), name='waybill_list'),
     path('waybill/add/', WayBillCreateView.as_view(), name='waybill_add'),
     path('waybill/<int:pk>/', WayBillDetailView.as_view(), name='waybill_detail'),
