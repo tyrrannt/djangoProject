80668c2b722cb2b94aa74bb6edceab0a24bd34df
msi <shakirov.vitaliy@gmail.com>
2024-04-11 09:23:19 +0300

    Update module

--- customers_app/consumers.py
+++ customers_app/consumers.py
@@ -3,17 +3,18 @@ import random
 from asyncio import sleep
 
 from channels.generic.websocket import AsyncWebsocketConsumer
-# from channels.auth import
+from channels.layers import get_channel_layer
 import json
 
 
 class EchoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
-
+        channel_layer = get_channel_layer()
         for i in range(1000):
             num = random.randint(0,1000)
             await self.send(json.dumps({"value": num}))
+            channel_layer.group_send('notifications', {})
             await sleep(1)
 
     def disconnect(self, close_code):
