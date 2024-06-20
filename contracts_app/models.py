9d5b22221e8294329b09e7a487244ed0a3005e99
msi <shakirov.vitaliy@gmail.com>
2024-06-20 16:08:06 +0300

    Update module

--- contracts_app/models.py
+++ contracts_app/models.py
@@ -157,7 +157,7 @@ class ContractModel(models.Model):
     executor = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Исполнитель', on_delete=models.SET_NULL,
                                  null=True,
                                  related_name='contract_executor')
-    doc_file = models.FileField(verbose_name='Файл документа', upload_to=contract_directory_path, blank=True)
+    doc_file = models.FileField(verbose_name='Файл документа', max_length=150, upload_to=contract_directory_path, blank=True)
     access = models.ForeignKey(AccessLevel, verbose_name='Уровень доступа к документу', on_delete=models.SET_NULL,
                                null=True, default=5)
     allowed_placed = models.BooleanField(verbose_name='Разрешение на публикацию', default=False)
