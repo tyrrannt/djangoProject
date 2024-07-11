168ad25a0e3ce2325a6091b30cc2c4efa8bc5d99
msi <shakirov.vitaliy@gmail.com>
2024-07-11 13:26:09 +0300

    Обновлены все пакеты

--- telegram_app/urls.py
+++ telegram_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from telegram_app.views import home
+
 app_name = 'telegram_app'
 urlpatterns = [
-    # path('', home, name='home'),
+    path('', home, name='home'),
     # path(WEBHOOK_PATH, telegram, name='webhook'),
 ]
--- telegram_app/views.py
+++ telegram_app/views.py
@@ -1,2 +1,30 @@
+import json
+
 from django.shortcuts import render
 
+
+def home(request):
+    dict_obj = {
+        '1': {
+            'first_name': 'Иванов',
+            'last_name': 'Сергей',
+            'surname': 'Андреевич',
+            'email': 'ivanov@barkol.ru',
+            'phone': '88008008080',
+            'address': 'Волгоград',
+            'birthday': '10.10.1980',
+            'gender': 'мужской',
+        },
+        '2': {
+            'first_name': 'Каплин',
+            'last_name': 'Михаил',
+            'surname': 'Вениаминович',
+            'email': 'kaplin@barkol.ru',
+            'phone': '88008008080',
+            'address': 'Москва',
+            'birthday': '10.10.1970',
+            'gender': 'мужской',
+        },
+    }
+    return render(request, 'telegram_app/home.html',
+                  context={'dict_obj': json.dumps(dict_obj, ensure_ascii=False, indent=4)})
