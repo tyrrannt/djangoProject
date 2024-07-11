459b4d024ce70e282826bcd9e84c5350c2ec3c77
msi <shakirov.vitaliy@gmail.com>
2024-07-11 15:10:02 +0300

    Обновлены все пакеты

--- telegram_app/views.py
+++ telegram_app/views.py
@@ -8,13 +8,14 @@ from customers_app.models import DataBaseUser
 
 
 def json_view(request):
-    dict_obj = DataBaseUser.objects.filter(is_active=False)
+    dict_obj = DataBaseUser.objects.filter(is_active=False)[:3]
     queryset = serializers.serialize('json', dict_obj,
-                                     fields=('first_name', 'last_name', 'surname', 'address', 'birthday', 'gender'))
+                                     fields=('last_name', 'first_name', 'surname', 'address', 'birthday', 'gender'))
     return HttpResponse(queryset, content_type='application/json')
 
 
 def xml_view(request):
-    dict_obj = DataBaseUser.objects.filter(is_active=False)
-    queryset = serializers.serialize('xml', dict_obj, fields=('first_name', 'last_name','surname', 'address', 'birthday', 'gender'))
+    dict_obj = DataBaseUser.objects.filter(is_active=False)[:3]
+    queryset = serializers.serialize('xml', dict_obj,
+                                     fields=('last_name', 'first_name', 'surname', 'address', 'birthday', 'gender'))
     return HttpResponse(queryset, content_type='application/xml')
