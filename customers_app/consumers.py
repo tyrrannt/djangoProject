d77f9f32f613d664cf358f9f5df99fdb6e7d98d7
msi <shakirov.vitaliy@gmail.com>
2024-11-20 17:25:49 +0300

    Update statics

--- customers_app/consumers.py
+++ customers_app/consumers.py
@@ -2,6 +2,7 @@
 import random
 from asyncio import sleep
 
+import emoji
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
 import json
@@ -88,6 +89,56 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         username = FIO_format(self.scope['user'].title)  # Получаем имя пользователя)
+
+        # Отправка сообщения в группу
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+            }
+        )
+
+    # Получение сообщения от группы
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        # Отправка сообщения в WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+        }))
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Присоединение к группе
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Покидание группы
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Получение сообщения от WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = FIO_format(self.scope['user'].title)  # Получаем имя пользователя
+
+        # Преобразуем текстовые смайлы в графические смайлы
+        message = emoji.emojize(message, language='alias')
 
         # Отправка сообщения в группу
         await self.channel_layer.group_send(
