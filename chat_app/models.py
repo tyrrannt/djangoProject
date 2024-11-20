db32f2927c8f4773f5a8a2489501f000aa296f6c
msi <shakirov.vitaliy@gmail.com>
2024-11-20 21:08:11 +0300

    Update statics

--- chat_app/models.py
+++ chat_app/models.py
@@ -2,6 +2,29 @@ from django.db import models
 
 # Create your models here.
 # from django.contrib.auth.models import User
+class BaseEmojiField:
+    def to_python(self, value):
+        if isinstance(value, str) or value is None:
+            if value is None:
+                return value
+            else:
+                encoded_string = str(value).encode('unicode_escape')
+                return encoded_string
+        return str(value)
+
+    def from_db_value(self, value, *args, **kwargs):
+        if str(value).startswith("b'"):
+            value = bytes(str(value).replace("b'", "")[0:-1].replace('\\\\', '\\'), 'utf-8').decode('unicode_escape')
+        elif str(value).isascii():
+            value = bytes(str(value), 'utf-8').decode('unicode_escape')
+            return value
+        return value
+
+class CustomEmojiCharField(BaseEmojiField , models.CharField):
+    pass
+
+class CustomEmojiTextField(BaseEmojiField ,models.TextField):
+    pass
 
 class Message(models.Model):
     class Meta:
@@ -9,7 +32,7 @@ class Message(models.Model):
         verbose_name_plural = 'Сообщения'
     room_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
-    message = models.TextField()
+    message = CustomEmojiTextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
