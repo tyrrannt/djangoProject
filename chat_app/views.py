2e2a23c3aee6c4d98f4ae20a74707005f32ad2b7
msi <shakirov.vitaliy@gmail.com>
2024-11-20 17:42:10 +0300

    Update statics

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+@login_required
 def room(request, room_name):
     return render(request, 'room.html', {
         'room_name': room_name
