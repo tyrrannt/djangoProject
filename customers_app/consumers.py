73c268795850d21c8468b0aa0eaca0d655d700c1
msi <shakirov.vitaliy@gmail.com>
2024-11-27 12:15:04 +0300

    Update statics

--- customers_app/consumers.py
+++ customers_app/consumers.py
@@ -10,6 +10,8 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
 import json
 
+from django.contrib.auth.models import AnonymousUser
+
 from chat_app.models import Message
 from contracts_app.templatetags.custom import FIO_format
 
@@ -21,12 +23,15 @@ class OnlineUsersConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         """Метод для установки соединения и добавления пользователя в список онлайн пользователей."""
-        await self.accept()  # Принимаем соединение
-        user = self.scope['user']  # Получаем объект пользователя из scope
-        if user.is_authenticated:  # Проверяем, авторизован ли пользователь
-            self.online_users.add(FIO_format(user.title))  # Добавляем имя пользователя в множество online_users
-            await self.channel_layer.group_add('online_users', self.channel_name)  # Добавляем соединение в группу online_users
-            await self.send_online_users()  # Отправляем список онлайн пользователей
+        if self.scope["user"] == AnonymousUser():
+            await self.close()
+        else:
+            await self.accept()  # Принимаем соединение
+            user = self.scope['user']  # Получаем объект пользователя из scope
+            if user.is_authenticated:  # Проверяем, авторизован ли пользователь
+                self.online_users.add(FIO_format(user.title))  # Добавляем имя пользователя в множество online_users
+                await self.channel_layer.group_add('online_users', self.channel_name)  # Добавляем соединение в группу online_users
+                await self.send_online_users()  # Отправляем список онлайн пользователей
 
     async def disconnect(self, close_code):
         """Метод для обработки разрыва соединения и удаления пользователя из списка онлайн пользователей."""
@@ -57,78 +62,81 @@ class OnlineUsersConsumer(AsyncWebsocketConsumer):
 
 
 
+# class ChatConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         self.room_name = self.scope['url_route']['kwargs']['room_name']
+#         self.room_group_name = 'chat_%s' % self.room_name
+#
+#         # Присоединение к группе
+#         await self.channel_layer.group_add(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+#
+#         await self.accept()
+#
+#     async def disconnect(self, close_code):
+#         # Покидание группы
+#         await self.channel_layer.group_discard(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+#
+#     # Получение сообщения от WebSocket
+#     async def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json['message']
+#         username = FIO_format(self.scope['user'].title)  # Получаем имя пользователя)
+#
+#         # Отправка сообщения в группу
+#         await self.channel_layer.group_send(
+#             self.room_group_name,
+#             {
+#                 'type': 'chat_message',
+#                 'message': message,
+#                 'username': username,
+#             }
+#         )
+#
+#     # Получение сообщения от группы
+#     async def chat_message(self, event):
+#         message = event['message']
+#         username = event['username']
+#
+#         # Отправка сообщения в WebSocket
+#         await self.send(text_data=json.dumps({
+#             'message': message,
+#             'username': username,
+#         }))
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        if self.scope["user"] == AnonymousUser():
+            await self.close()
+        else:
+            self.room_name = self.scope['url_route']['kwargs']['room_name']
+            self.room_group_name = 'chat_%s' % self.room_name
 
-        # Присоединение к группе
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+            # Присоединение к группе
+            await self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name
+            )
 
-        await self.accept()
+            await self.accept()
 
-    async def disconnect(self, close_code):
-        # Покидание группы
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+            # Отправка уведомления о подключении пользователя
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'user_notification',
+                    'message': f'{self.scope["user"].username} подключился к чату',
+                    'username': 'Система',
+                }
+            )
 
-    # Получение сообщения от WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        username = FIO_format(self.scope['user'].title)  # Получаем имя пользователя)
-
-        # Отправка сообщения в группу
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'username': username,
-            }
-        )
-
-    # Получение сообщения от группы
-    async def chat_message(self, event):
-        message = event['message']
-        username = event['username']
-
-        # Отправка сообщения в WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message,
-            'username': username,
-        }))
-
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-
-        # Присоединение к группе
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
-
-        # Отправка уведомления о подключении пользователя
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'user_notification',
-                'message': f'{self.scope["user"].username} подключился к чату',
-                'username': 'Система',
-            }
-        )
-
-        # # Загрузка истории чата
-        # await self.load_chat_history()
+            # # Загрузка истории чата
+            # await self.load_chat_history()
 
     async def disconnect(self, close_code):
         # Отправка уведомления об отключении пользователя
@@ -210,35 +218,38 @@ class MonitorConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         """Метод для установки соединения и отправки данных о загрузке процессора, памяти, диска, сетевого трафика, количества процессов и сетевых соединений каждую секунду."""
-        await self.accept()  # Принимаем соединение
-        while True:
-            cpu_percent = psutil.cpu_percent(interval=1)  # Получаем процент загрузки процессора
-            memory_percent = psutil.virtual_memory().percent  # Получаем процент загрузки памяти
-            disk_percent = psutil.disk_usage('/').percent  # Получаем процент загрузки диска
-            net_io = psutil.net_io_counters()  # Получаем информацию о сетевом трафике
-            net_sent = net_io.bytes_sent  # Получаем количество отправленных байт
-            net_recv = net_io.bytes_recv  # Получаем количество полученных байт
-            processes = len(psutil.pids())  # Получаем количество процессов
-            connections = len(psutil.net_connections())  # Получаем количество сетевых соединений
+        if self.scope["user"] == AnonymousUser():
+            await self.close()
+        else:
+            await self.accept()  # Принимаем соединение
+            while True:
+                cpu_percent = psutil.cpu_percent(interval=1)  # Получаем процент загрузки процессора
+                memory_percent = psutil.virtual_memory().percent  # Получаем процент загрузки памяти
+                disk_percent = psutil.disk_usage('/').percent  # Получаем процент загрузки диска
+                net_io = psutil.net_io_counters()  # Получаем информацию о сетевом трафике
+                net_sent = net_io.bytes_sent  # Получаем количество отправленных байт
+                net_recv = net_io.bytes_recv  # Получаем количество полученных байт
+                processes = len(psutil.pids())  # Получаем количество процессов
+                connections = len(psutil.net_connections())  # Получаем количество сетевых соединений
 
-            # Получение температуры процессора (работает на Linux)
-            if os.name == 'posix':
-                temps = psutil.sensors_temperatures()
-                cpu_temp = temps['coretemp'][0].current if 'coretemp' in temps else None
-            else:
-                cpu_temp = None
+                # Получение температуры процессора (работает на Linux)
+                if os.name == 'posix':
+                    temps = psutil.sensors_temperatures()
+                    cpu_temp = temps['coretemp'][0].current if 'coretemp' in temps else None
+                else:
+                    cpu_temp = None
 
-            await self.send(text_data=json.dumps({
-                'cpu_percent': cpu_percent,  # Отправляем данные о загрузке процессора
-                'memory_percent': memory_percent,  # Отправляем данные о загрузке памяти
-                'disk_percent': disk_percent,  # Отправляем данные о загрузке диска
-                'net_sent': converter(net_sent),  # Отправляем данные о количестве отправленных байт
-                'net_recv': converter(net_recv),  # Отправляем данные о количестве полученных байт
-                'processes': processes,  # Отправляем данные о количестве процессов
-                'connections': connections,  # Отправляем данные о количестве сетевых соединений
-                'cpu_temp': cpu_temp,  # Отправляем данные о температуре процессора
-            }))
-            await sleep(1)  # Ждем 1 секунду перед отправкой следующих данных
+                await self.send(text_data=json.dumps({
+                    'cpu_percent': cpu_percent,  # Отправляем данные о загрузке процессора
+                    'memory_percent': memory_percent,  # Отправляем данные о загрузке памяти
+                    'disk_percent': disk_percent,  # Отправляем данные о загрузке диска
+                    'net_sent': converter(net_sent),  # Отправляем данные о количестве отправленных байт
+                    'net_recv': converter(net_recv),  # Отправляем данные о количестве полученных байт
+                    'processes': processes,  # Отправляем данные о количестве процессов
+                    'connections': connections,  # Отправляем данные о количестве сетевых соединений
+                    'cpu_temp': cpu_temp,  # Отправляем данные о температуре процессора
+                }))
+                await sleep(1)  # Ждем 1 секунду перед отправкой следующих данных
 
     async def disconnect(self, close_code):
         """Метод для обработки разрыва соединения."""
