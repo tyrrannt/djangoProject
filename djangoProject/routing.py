d2fd8c379b129e04f223854f5ffd486471252079
msi <shakirov.vitaliy@gmail.com>
2025-11-15 13:10:43 +0300

    Added lpi

--- djangoProject/routing.py
+++ djangoProject/routing.py
@@ -6,7 +6,7 @@ from customers_app import consumers
 
 websocket_urlpatterns = [
     path(r'ws/online_users/', consumers.OnlineUsersConsumer.as_asgi()),
-    path(r'ws/private/$', consumers.PrivateMessageConsumer.as_asgi()),
+    path(r'ws/private/', consumers.PrivateMessageConsumer.as_asgi()),
     path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
     path('ws/monitor/', consumers.MonitorConsumer.as_asgi()),
     path('ws/video_conference/<str:room_name>/', consumers.VideoConferenceConsumer.as_asgi()),
