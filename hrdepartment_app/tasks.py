f61aedd68f0a39f218e78c68a2386af3bcdd6590
msi <it-head@barkol.ru>
2025-12-08 11:33:53 +0300

    new commit

--- hrdepartment_app/tasks.py
+++ hrdepartment_app/tasks.py
@@ -16,7 +16,7 @@ from django.db import transaction
 from django.db.models import Q
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-from loguru import logger
+from core import logger
 from administration_app.utils import (
     get_jsons_data_filter2,
     get_date_interval,
