b94c429e6b5061de57ad2eaf47bf9393c63b844f
msi <it-head@barkol.ru>
2026-09-04 18:40:00 +0300

    Масштабирование пула воркеров Gunicorn под аппаратную конфигурацию 8 vCPU / 8 GB RAM

--- djangoProject/gunicorn.conf.py
+++ djangoProject/gunicorn.conf.py
@@ -1,7 +1,7 @@
-# gunicorn.conf.py - Оптимизированная конфигурация для нагруженного сервера
+# gunicorn.conf.py - Оптимизированная конфигурация для нагруженного сервера (8 vCPU / 8 GB RAM)
 
 bind = '127.0.0.1:8000'
-workers = 5
+workers = 8
 threads = 4
 worker_class = 'gthread'
 worker_connections = 1000
